986e7e526566e0c7fd730cdbf797b9bcc1865239
DHPR <133486266+Deepking88@users.noreply.github.com>
2024-07-27 11:36:51 +0530

    Update DeepAi.py

--- DaxxAi.py
+++ DaxxAi.py
@@ -17,8 +17,8 @@ import os
 import re
 
 
-API_ID = "6435225"
-API_HASH = "4e984ea35f854762dcde906dce426c2d"
+API_ID = "28362125"
+API_HASH = "c750e5872a2af51801d9b449983f4c84"
 SESSION_NAME = os.environ.get("SESSION_NAME", "")
 MONGO_URL = os.environ.get("MONGO_URL", "")
 
